d0f2400fa0a3c0dba1b1799476d6537daf771dfa
Ala Raddaoui <raddaoui.ala@intel.com>
2018-06-12 01:53:15 -0700

    fixed cicd image to accept wf chart and config postsubmit job

--- images/ci_image/run_workflows.sh
+++ images/ci_image/run_workflows.sh
@@ -3,7 +3,7 @@
 # This script is meant to be the entrypoint for a prow job.
 # It checkos out a repo and then looks for prow_config.yaml in that
 # repo and uses that to run one or more workflows.
-set -eux
+set -ex
 
 # Checkout the code.
 /usr/local/bin/checkout.sh /src
@@ -13,7 +13,7 @@ cd /src/${REPO_OWNER}/${REPO_NAME}/helm-charts
 # create a valid argo workflow name 
 workflowname=`echo ${REPO_NAME}-${BUILD_NUMBER} | tr 'A-Z_' 'a-z-'`
 # run testing_workflow in the repo
-helm install --set workflowname=${workflowname} --set pull_number=${PULL_NUMBER} --set repo_name=${REPO_NAME} --set repo_owner=${REPO_OWNER} --set build_number=${BUILD_NUMBER} testing_workflow/
+helm install --set workflowname=${workflowname} --set pull_number=${PULL_NUMBER} --set repo_name=${REPO_NAME} --set repo_owner=${REPO_OWNER} --set build_number=${BUILD_NUMBER} $workflow_chart/
 
 # wait till workflow completes running
 argo_status=$(argo get  ${workflowname} -o json | jq .status.phase)
